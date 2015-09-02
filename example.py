542fc2fa90d08215aca47dd36105212ecbf5dfbf
amirouche <amirouche.boubekki@gmail.com>
2015-09-03 00:55:08 +0200

    small change in the example too lazy to make a good one

--- example.py
+++ example.py
@@ -3,17 +3,18 @@ from wiredtiger_ffi import Wiredtiger
 # init
 wiredtiger = Wiredtiger('tmp', 'create')
 session = wiredtiger.open_session()
-session.create('table:test', "key_format=q,value_format=q")
+session.create('table:test', "key_format=Q,value_format=QQ")
 cursor = session.open_cursor('table:test')
 
 print('key format', cursor._key_format())
 print('value format', cursor._value_format())
+
 # insert
-cursor.set_key(-123)
-print('key is', cursor.get_key())
-cursor.set_value(-456)
-print('value is', cursor.get_value())
+cursor.set_key(123)
+cursor.set_value(456, 789)
 cursor.insert()
+print('key is', cursor.get_key())
+print('value is', cursor.get_value())
 cursor.reset()
 
 # get
