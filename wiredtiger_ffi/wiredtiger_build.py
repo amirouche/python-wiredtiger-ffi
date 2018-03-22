a979c5678665477389df95d0c61b56f450536f34
amirouche <amirouche+dev@hypermove.net>
2018-03-22 17:01:33 +0100

    cosmit

--- wiredtiger_ffi/wiredtiger_build.py
+++ wiredtiger_ffi/wiredtiger_build.py
@@ -7,5 +7,6 @@ ffi.set_source("_wiredtiger", None)
 with open(os.path.join(os.path.dirname(__file__), 'wiredtiger.h')) as f:
     ffi.cdef(f.read())
 
+
 if __name__ == '__main__':
     ffi.compile()
